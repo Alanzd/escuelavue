1bffa8920687143c5053a5c471be1ae261351c77
Ana_Lanz <ana.lanz.d@gmail.com>
2024-11-19 16:23:59 +0100

    renderizado condicional, errores

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const API = "https://api.github.com/users/"
+const API = "https://api.github.com/users/";
 
 const app = Vue.createApp({
   data () {
     return {
-      message: 'Hola'
-    };
-  },    
-})
+      search: null,
+      result: null
+    }
+  },
+  methods: {
+    async doSearch() {
+      
+      try {
+        const response = await fetch(API + this.search )
+        const data = await response.json(); 
+        if(!response.ok) throw new Error('user not found') 
+        console.log(data);
+        this.result = data
+        
+      } catch (error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
+           
+    }
+  }, 
+});
 
