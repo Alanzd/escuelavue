11214c8858e55e4c2f95762ea4bd20c80f665f01
Ana_Lanz <ana.lanz.d@gmail.com>
2024-11-20 11:21:55 +0100

    renderizado de listas

--- js/main.js
+++ js/main.js
@@ -4,18 +4,30 @@ const app = Vue.createApp({
   data () {
     return {
       search: null,
-      result: null
+      result: null, 
+      error: null,
+      favorites: new Map()
+    }
+  },
+  computed: {
+    isFavorite() {
+      return this.favorites.has(this.result.id)
+    },
+
+    // con este metodo solo obtengo los valores de favoritos
+    allFavorites() {
+      return Array.from (this.favorites.values())
     }
   },
   methods: {
     async doSearch() {
-      
+      // reinicialiamos los estados cada vez que hacemos la peticion
+      this.result = this.error = null
       try {
         const response = await fetch(API + this.search )
         const data = await response.json(); 
         if(!response.ok) throw new Error('user not found') 
-        console.log(data);
-        this.result = data
+        this.result = data  
         
       } catch (error) {
         this.error = error
@@ -23,6 +35,16 @@ const app = Vue.createApp({
         this.search = null
       }
            
+    },
+
+    // seteamos la clave favorites, le añadimos la clave id y el valor será un objeto con todo el 
+    //  resultado para ese id. Ej: 1657: {name: Ana Lanz, bio: blablabla....}
+    // los mapas garantizan que los valores son unicos, no se duplican
+    addFavorite() {
+      this.favorites.set(this.result.id, this.result)
+    },
+     removeFavorite() {
+      this.favorites.delete(this.result.id)
     }
   }, 
 });
